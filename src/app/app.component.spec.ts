f4952c58643bff3c601c9ee70824d3ac8a606137
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2019-10-18 16:55:43 -0300

    Creating struct for todo list

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,36 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { Todo } from './todo';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [
+				FormsModule
+			],
+			declarations: [
+				AppComponent
+			],
+		});
+	});
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+	it('should create the app', async(() => {
+		let fixture = TestBed.createComponent(AppComponent);
+		let app = fixture.debugElement.componentInstance;
+		expect(app).toBeTruthy();
+	}));
 
-  it(`should have as title 'angularAPP'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angularAPP');
-  });
+	it(`should have a newTodo todo`, async(() => {
+		let fixture = TestBed.createComponent(AppComponent);
+		let app = fixture.debugElement.componentInstance;
+		expect(app.newTodo instanceof Todo).toBeTruthy()
+	}));
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angularAPP app is running!');
-  });
+	it("should display 'Todos' in h1 tag", async(() => {
+		let fixture = TestBed.createComponent(AppComponent);
+		fixture.detectChanges();
+		let compiled = fixture.debugElement.nativeElement;
+		expect(compiled.querySelector('h1').textContent).toContain('Todos');
+	}));
 });
